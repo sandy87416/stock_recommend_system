4f1a909465c39c8836f97aba3c922ed6b2e7742e
109598053 <t109598053@ntut.org.tw>
2022-04-25 22:35:18 +0800

    added Admin model
    account and password

---
+++ model/member/admin.py
@@ -0,0 +1,16 @@
+class Admin:
+    __account = ''
+    __password = ''
+
+    def __init__(self, account, password):
+        self.__account = account
+        self.__password = password
+
+    def get_account(self):
+        return self.__account
+
+    def get_password(self):
+        return self.__password
+
+    def set_password(self, password):
+        self.__password = password
