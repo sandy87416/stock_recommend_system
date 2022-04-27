dc0cd2e065812731bd2f486f05f5bc6b6e7f65ef
109598087 <69856059+109598087@users.noreply.github.com>
2022-04-27 23:17:21 +0800

    revised OrdinaryMember model

--- model/member/ordinary_member.py
+++ model/member/ordinary_member.py
@@ -3,6 +3,7 @@ import pandas as pd
 from config import database_path
 from model.member.application_information import ApplicationInformation
 from model.member.member import Member
+from model.utils import update_database
 
 
 class OrdinaryMember(Member):
@@ -10,32 +11,23 @@ class OrdinaryMember(Member):
         super().__init__(account, password)
         self.__account = account
         self.__password = password
-        member_df = pd.read_csv(database_path + 'member/member.csv')
-        member_df = pd.concat([member_df, pd.DataFrame({
-            'account': [account],
-            'password': [password],
-            'level': [2],
-        })])
-        member_df.to_csv(database_path + 'member/member.csv', index=False)
+        self.__application_information = None
 
     def create_application_information(self):
-        application_information_df = pd.read_csv(
-            database_path + 'member/application_information.csv')
-        application_information_df = pd.concat([application_information_df, pd.DataFrame({
-            'account': [self.get_account()],
-            'content': [''],
-        })])
-        application_information_df.to_csv(database_path + 'member/application_information.csv',
-                                          index=False)
-        return ApplicationInformation(self.get_account(), '')
+        self.__application_information = ApplicationInformation(self.get_account(), '')
 
     def apply_premium_member(self, content):
-        self.create_application_information()
-        application_information_df = pd.read_csv(
-            database_path + 'member/application_information.csv')
-        row_index = application_information_df[application_information_df['account'] == self.get_account()].index
-        column_index = application_information_df.columns.get_loc("content")
-        application_information_df.iloc[row_index, column_index] = content
-        application_information_df.to_csv(database_path + 'member/application_information.csv',
-                                          index=False)
+        self.__application_information.set_content(content)
+        apply_account = self.__application_information.get_account()
+        apply_content = self.__application_information.get_content()
+
+        df = pd.read_csv(database_path + 'member/application_information.csv')
+        df = pd.concat([df, pd.DataFrame({
+            'account': [apply_account],
+            'content': [apply_content],
+        })])
+        df.to_csv(database_path + 'member/application_information.csv', index=False)
         return 'Success'
+
+    def get_application_information(self):
+        return self.__application_information
--- test/test_model/test_member/test_ordinary_member.py
+++ test/test_model/test_member/test_ordinary_member.py
@@ -7,18 +7,17 @@ from model.member.ordinary_member import OrdinaryMember
 
 
 class TestOrdinaryMember(TestCase):
-
-    @classmethod
-    def setUpClass(self):
-        self.ordinary_member = OrdinaryMember("t109598087@ntut.org.tw", 'islab1221')
+    def setUp(self):
+        self.ordinary_member = OrdinaryMember("t109598087@ntut.org.tw", 'islab')
 
     def test_get_account(self):
         self.assertEqual(self.ordinary_member.get_account(), 't109598087@ntut.org.tw')
 
     def test_get_password(self):
-        self.assertEqual(self.ordinary_member.get_password(), 'islab1221')
+        self.assertEqual(self.ordinary_member.get_password(), 'islab')
 
     def test_set_password(self):
+        self.assertEqual(self.ordinary_member.get_password(), 'islab')
         self.ordinary_member.set_password('802138')
         self.assertEqual(self.ordinary_member.get_password(), '802138')
 
@@ -29,43 +28,30 @@ class TestOrdinaryMember(TestCase):
         # teardown
         member_df.iloc[
             member_df[member_df['account'] == self.ordinary_member.get_account()].index, member_df.columns.get_loc(
-                "password")] = "islab1221"
+                "password")] = "islab"
         member_df.to_csv(database_path + 'member/member.csv', index=False)
 
     def test_create_application_information(self):
-        application_information = self.ordinary_member.create_application_information()
-        self.assertEqual(application_information.get_content(), '')
-        application_information_df = pd.read_csv(
-            database_path + 'member/application_information.csv')
-        account = application_information_df.tail(1)['account'].to_numpy()[0]
-        self.assertEqual(account, "t109598087@ntut.org.tw")
+        self.assertEqual(self.ordinary_member.get_application_information(), None)
+        self.ordinary_member.create_application_information()
 
-        # teardown
-        application_information_df = application_information_df.drop(
-            application_information_df[
-                application_information_df['account'] == self.ordinary_member.get_account()].index)
-        application_information_df.to_csv(database_path + 'member/application_information.csv',
-                                          index=False)
+        application_information = self.ordinary_member.get_application_information()
+        self.assertNotEqual(application_information, None)
+        self.assertEqual(application_information.get_account(), self.ordinary_member.get_account())
+        self.assertEqual(application_information.get_content(), '')
 
     def test_apply_premium_member(self):
+        self.ordinary_member.create_application_information()
         apply_result = self.ordinary_member.apply_premium_member("I want to be the premium member.")
         self.assertEqual(apply_result, 'Success')
-        application_information_df = pd.read_csv(
-            database_path + 'member/application_information.csv')
-        content = \
-            application_information_df[application_information_df['account'] == self.ordinary_member.get_account()][
-                'content'].to_numpy()[0]
+
+        application_information = self.ordinary_member.get_application_information()
+        self.assertEqual(application_information.get_content(), "I want to be the premium member.")
+
+        apply_df = pd.read_csv(database_path + 'member/application_information.csv')
+        content = apply_df[apply_df['account'] == application_information.get_account()]['content'].to_numpy()[0]
         self.assertEqual(content, "I want to be the premium member.")
 
         # teardown
-        application_information_df = application_information_df.drop(application_information_df[
-                                                                         application_information_df[
-                                                                             'account'] == self.ordinary_member.get_account()].index)
-        application_information_df.to_csv(database_path + 'member/application_information.csv',
-                                          index=False)
-
-    @classmethod
-    def tearDownClass(self) -> None:
-        member_df = pd.read_csv(database_path + 'member/member.csv')
-        member_df = member_df.drop(member_df[member_df['account'] == "t109598087@ntut.org.tw"].index)
-        member_df.to_csv(database_path + 'member/member.csv', index=False)
+        apply_df = apply_df.drop(apply_df[apply_df['account'] == self.ordinary_member.get_account()].index)
+        apply_df.to_csv(database_path + 'member/application_information.csv', index=False)
