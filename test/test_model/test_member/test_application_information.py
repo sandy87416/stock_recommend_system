742ddf32e6e972b797d5e39ccaa81f1e3fc4ef33
109598053 <t109598053@ntut.org.tw>
2022-04-27 16:37:43 +0800

    added application information database
    added application_information.csv
    added application information database test
    refactored ApplicationInformation model

--- test/test_model/test_member/test_application_information.py
+++ test/test_model/test_member/test_application_information.py
@@ -1,12 +1,18 @@
+import csv
+import pandas as pd
 from unittest import TestCase
 
+
+from config import database_path
 from model.member.application_information import ApplicationInformation
 
 
 class TestApplicationInformation(TestCase):
     @classmethod
     def setUpClass(self):
-        self.application_information = ApplicationInformation("t109598053@ntut.org.tw", "administrator")
+        self.account = "t109598053@ntut.org.tw"
+        self.content = "administrator"
+        self.application_information = ApplicationInformation(self.account, self.content)
 
     def test_get_account(self):
         self.assertEqual(self.application_information.get_account(), "t109598053@ntut.org.tw")
@@ -15,5 +21,16 @@ class TestApplicationInformation(TestCase):
         self.assertEqual(self.application_information.get_content(), "administrator")
 
     def test_set_content(self):
+        self.assertEqual(self.application_information.get_content(), "administrator")
+
         self.application_information.set_content("I want to be the premium member.")
         self.assertEqual(self.application_information.get_content(), "I want to be the premium member.")
+
+        application_info_df = pd.read_csv(database_path + 'member/application_information.csv')
+        new_content = application_info_df[application_info_df['account'] == self.account]['content'].to_numpy()[0]
+        self.assertEqual(new_content, "I want to be the premium member.")
+
+        # teardown
+        teardown_index = application_info_df[application_info_df['account'] == self.account].index
+        application_info_df.iloc[teardown_index, application_info_df.columns.get_loc("content")] = self.content
+        application_info_df.to_csv(database_path + 'member/application_information.csv', index=False)
