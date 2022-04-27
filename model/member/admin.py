41774116e8b0b9b552d1d5782f2a4aaf08e00d5d
109598053 <t109598053@ntut.org.tw>
2022-04-27 16:38:26 +0800

    refactored Admin model
    refactored Admin model

--- model/member/admin.py
+++ model/member/admin.py
@@ -11,21 +11,25 @@ class Admin:
         self.__account = account
         self.__password = password
 
-
     def get_account(self):
         return self.__account
 
     def get_password(self):
         return self.__password
 
-    def set_password(self, password):
-        self.__password = password
+    @staticmethod
+    def __save_password_to_database(account, password):
         member_df = pd.read_csv(database_path + 'member/member.csv')
-        member_df.iloc[member_df[member_df['account'] == self.get_account()].index, member_df.columns.get_loc("password")] = password
+        member_index = member_df[member_df['account'] == account].index
+        member_df.iloc[member_index, member_df.columns.get_loc("password")] = password
         member_df.to_csv(database_path + 'member/member.csv', index=False)
 
+    def set_password(self, password):
+        self.__password = password
+        self.__save_password_to_database(self.__account, self.__password)
 
-    def upgrade_member_level(self, account):
+    @staticmethod
+    def upgrade_member_level(account):
         member_df = pd.read_csv(database_path + 'member/member.csv')
         level = member_df[member_df['account'] == account]['level'].to_numpy()[0]
         if level == 2:
