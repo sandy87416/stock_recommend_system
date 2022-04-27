0d8655e096065332f46f96cf54b3e95596677547
109598087 <69856059+109598087@users.noreply.github.com>
2022-04-27 23:17:50 +0800

    added update_database

--- model/utils.py
+++ model/utils.py
@@ -4,6 +4,9 @@ from config import database_path
 
 
 def update_database(path, key_name, key, value_name, value):
-    member_df = pd.read_csv(database_path + path)
-    member_df.iloc[member_df[member_df[key_name] == key].index, member_df.columns.get_loc(value_name)] = value
-    member_df.to_csv(database_path + path, index=False)
+    df = pd.read_csv(database_path + path)
+    df.iloc[df[df[key_name] == key].index, df.columns.get_loc(value_name)] = value
+    df.to_csv(database_path + path, index=False)
+
+
+
