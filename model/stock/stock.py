73234ffcad887a7fc5b807f06fdfcb4d096da536
pc092 <t109598092@ntut.org.tw>
2022-04-27 10:49:21 +0800

    added Stock model

--- model/stock/stock.py
+++ model/stock/stock.py
@@ -1,3 +1,27 @@
+import pandas as pd
+import numpy as np
+
+from model.stock.intra_day_information import IntraDayInformation
+
+
+def is_nan(num):
+    return num != num
+
+
+def get_rsi(days_up_down_list):
+    up_mean = np.sum([up_down for up_down in days_up_down_list if up_down > 0])
+    down_mean = -np.sum([up_down for up_down in days_up_down_list if up_down < 0])
+    return round(up_mean / (up_mean + down_mean) * 100, 2)
+
+
+def get_mean_price_list(end_price_list, days):
+    mean_price_list = [0 for _ in range(days - 1)]
+    for i in range(len(end_price_list) - (days - 1)):
+        days_end_price_list = end_price_list[i:(i + days)]
+        mean_price_list.append(round(np.mean(days_end_price_list) + 0, 2))
+    return mean_price_list
+
+
 class Stock:
     __stock_id = 0
     __stock_name = ''
@@ -22,32 +46,59 @@ class Stock:
     def set_stock_name(self, stock_name):
         self.__stock_name = stock_name
 
-    # def get_stock_company_name(self):
-    #     return self.__stock_company_name
-    #
-    # def set_stock_company_name(self, stock_company_name):
-    #     self.__stock_id = stock_company_name
-    #
-    # def get_stock_classification(self):
-    #     return self.__stock_classification
-    #
-    # def set_stock_classification(self, stock_classification):
-    #     self.__stock_classification = stock_classification
-    #
-    # def get_stock_after_hours_information(self, stock_id):
-    #     pass
-    #
-    # def get_stock_intraday_information(self, stock_id):
-    #     pass
-    #
-    # def create_stock_intraday_information(self, stock_id):
-    #     pass
-    #
-    # def get_end_price(self, stock_id):
-    #     pass
-
     def get_stock_company_name(self):
         return self.__stock_company_name
 
     def set_stock_company_name(self, stock_company_name):
         self.__stock_company_name = stock_company_name
+
+    def get_stock_classification(self):
+        return self.__stock_classification
+
+    def set_stock_classification(self, stock_classification):
+        self.__stock_classification = stock_classification
+
+    def get_stock_after_hours_information(self, stock_id):
+        df = pd.read_csv('D:/ooad/stock_recommend_system/database/' + str(stock_id) + '.csv')
+        df = df.sort_values(['Date'], ascending=[False])
+        news_df = pd.read_csv('D:/ooad/stock_recommend_system/database/news.csv')
+        news_df = news_df.sort_values(['date', 'time'], ascending=[False, False])
+        news_list = news_df[news_df['stock_id'] == stock_id].to_numpy()
+        monthly_revenue_df = pd.read_csv('D:/ooad/stock_recommend_system/database/month_revenue.csv')
+        monthly_revenue_df = monthly_revenue_df[monthly_revenue_df['stock_id'] == stock_id]
+
+        date = df['Date'].to_numpy()[0]
+        k_value = ''
+        ma20_value = get_mean_price_list(df['Close'].to_numpy(), 20)[0]
+        rsi_value = get_rsi(df['Close'].to_numpy())
+        foreign_buy = '0' if is_nan(df['foreign_buy'].to_numpy()[0]) else df['foreign_buy'].to_numpy()[0]
+        investment_trust_buy = '0' if is_nan(df['investment_trust_buy'].to_numpy()[0]) else df['investment_trust_buy'].to_numpy()[0]
+        self_buy = '0' if is_nan(df['self_buy'].to_numpy()[0]) else df['self_buy'].to_numpy()[0]
+        news = news_list[0] if len(news_list) >= 1 else ''
+        monthly_revenue = monthly_revenue_df['month_revenue'].to_numpy()[0]
+        return date, k_value, ma20_value, rsi_value, foreign_buy, investment_trust_buy, self_buy, news, monthly_revenue
+
+    def get_stock_intraday_information(self, stock_id):
+        df = pd.read_csv('D:/ooad/stock_recommend_system/database/' + str(stock_id) + '.csv')
+        df = df.sort_values(by="Date", ascending=[False])
+        end_price = round(df['Close'].to_numpy()[0], 2)
+        min_price = round(df['Low'].to_numpy()[0], 2)
+        max_price = round(df['High'].to_numpy()[0], 2)
+        start_price = round(df['Open'].to_numpy()[0], 2)
+        return end_price, min_price, max_price, start_price
+
+    def create_stock_intraday_information(self, stock_id):
+        df = pd.read_csv('D:/ooad/stock_recommend_system/database/' + str(stock_id) + '.csv')
+        df = df.sort_values(by="Date", ascending=[False])
+        end_price = round(df['Close'].to_numpy()[0], 2)
+        min_price = round(df['Low'].to_numpy()[0], 2)
+        max_price = round(df['High'].to_numpy()[0], 2)
+        start_price = round(df['Open'].to_numpy()[0], 2)
+        return IntraDayInformation(end_price, min_price, max_price, start_price)
+
+    def get_end_price(self, stock_id):
+        df = pd.read_csv('D:/ooad/stock_recommend_system/database/' + str(stock_id) + '.csv')
+        df = df.sort_values(by="Date", ascending=[False])
+        end_price = round(df['Close'].to_numpy()[0], 2)
+        return end_price
+
--- test/test_model/test_stock/test_stock.py
+++ test/test_model/test_stock/test_stock.py
@@ -30,19 +30,39 @@ class TestStock(TestCase):
         self.assertEqual(self.stock.get_stock_company_name(), '亞洲水泥')
 
     def test_get_stock_classification(self):
-        pass
+        self.assertEqual(self.stock.get_stock_classification(), '水泥工業')
 
     def test_set_stock_classification(self):
-        pass
+        self.stock.set_stock_classification('食品工業')
+        self.assertEqual(self.stock.get_stock_classification(), '食品工業')
 
     def test_get_stock_after_hours_information(self):
-        pass
+        date, k_value, ma20_value, rsi_value, foreign_buy, investment_trust_buy, self_buy, news, monthly_revenue = \
+            self.stock.get_stock_after_hours_information(1215)
+        self.assertEqual(date, '2022-04-18')
+        self.assertEqual(k_value, '')
+        self.assertEqual(ma20_value, 0)
+        self.assertEqual(rsi_value, 100.0)
+        self.assertEqual(foreign_buy, '0')
+        self.assertEqual(investment_trust_buy, '0')
+        self.assertEqual(self_buy, '0')
+        self.assertEqual(news, '')
+        self.assertEqual(monthly_revenue, 5718985)
 
     def test_get_stock_intraday_information(self):
-        pass
+        end_price, min_price, max_price, start_price = self.stock.get_stock_intraday_information(1215)
+        self.assertEqual(end_price, 81.2)
+        self.assertEqual(min_price, 81.1)
+        self.assertEqual(max_price, 83.3)
+        self.assertEqual(start_price, 83.3)
 
     def test_create_stock_intraday_information(self):
-        pass
+        stock_intraday_information = self.stock.create_stock_intraday_information(self.stock.get_stock_id())
+        self.assertEqual(stock_intraday_information.get_end_price(), 46.5)
+        self.assertEqual(stock_intraday_information.get_min_price(), 46.5)
+        self.assertEqual(stock_intraday_information.get_max_price(), 47.3)
+        self.assertEqual(stock_intraday_information.get_start_price(), 47.3)
 
     def test_get_end_price(self):
-        pass
+        end_price = self.stock.get_end_price(1215)
+        self.assertEqual(end_price, 81.2)
