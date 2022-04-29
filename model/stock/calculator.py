6e5fb0b5329b15e7d8b23dd7bd88ade65df28262
109598087 <69856059+109598087@users.noreply.github.com>
2022-04-29 20:24:32 +0800

    added read_recommended_stock

--- model/stock/calculator.py
+++ model/stock/calculator.py
@@ -1,3 +1,8 @@
+import pandas as pd
+
+from config import database_path
+
+
 class Calculator:
 
     def __init__(self):
@@ -9,3 +14,28 @@ class Calculator:
         tax = 0.3 / 100
         fee_and_tax = (buy_price * fee + sell_price * tax + sell_price * fee)
         return round((sell_price - buy_price - fee_and_tax) * trading_volume)
+
+    def read_recommended_stock(self, days, odds):
+        days = int(days)
+        odds = float(odds) / 10
+
+        very_good_df = pd.read_csv(database_path + '' + str(days) + 'now.csv')
+        # send_to_line
+        very_good_df = very_good_df[(very_good_df['odds'] > odds)]  # 可修改
+
+        very_good_df = very_good_df.sort_values('odds', ascending=False)
+
+        # print
+        to_front_end_message_list = list()
+        very_good_stock_id_np = very_good_df['stock_id'].to_numpy()
+        very_good_stock_stock_name_np = very_good_df['stock_name'].to_numpy()
+        very_good_stock_rsi_np = very_good_df['rsi_6'].to_numpy()
+        very_good_stock_win_rate_np = very_good_df['odds'].to_numpy()
+        for i in range(len(very_good_df)):
+            print_str = ''
+            print_str += str(very_good_stock_id_np[i])
+            print_str += very_good_stock_stock_name_np[i] + ' '
+            print_str += 'rsi_6:' + str(very_good_stock_rsi_np[i]) + ' '
+            print_str += 'win_rate:' + str(round(very_good_stock_win_rate_np[i], 2))
+            to_front_end_message_list.append(print_str)
+        return to_front_end_message_list
--- test/test_model/test_stock/test_calculator.py
+++ test/test_model/test_stock/test_calculator.py
@@ -18,3 +18,12 @@ class TestCalculator(TestCase):
 
         profit_and_loss = self.calculator.calculate_profit_and_loss(600, 605, trading_volume=1000, securities_firm=0.2)
         self.assertEqual(profit_and_loss, 2842)
+
+    def test_read_recommended_stock(self):
+        to_front_end_message_list = self.calculator.read_recommended_stock(2, 0.8)
+        self.assertEqual(to_front_end_message_list[0], '8080永利聯合 rsi_6:100.0 win_rate:0.93')
+        self.assertEqual(to_front_end_message_list[1], '3434哲固 rsi_6:95.0 win_rate:0.93')
+
+        # to_front_end_message_list = self.calculator.read_recommended_stock(5, 0.8)
+        # self.assertEqual(to_front_end_message_list[0], '3712永崴投控 rsi_6:100.0 win_rate:0.92')
+        # self.assertEqual(to_front_end_message_list[1], '3434哲固 rsi_6:96.61 win_rate:0.91')
