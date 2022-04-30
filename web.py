823843a5c78e78779df2e752fafe0999fea4e47d
109598087 <69856059+109598087@users.noreply.github.com>
2022-04-30 10:21:57 +0800

    added use case read_stock_odds

--- web.py
+++ web.py
@@ -23,13 +23,26 @@ def query_recommended_stock_page():
     return render_template('query_recommended_stock_page.html')
 
 
-@app.route('/read_recommend_stock_odds', methods=['GET', 'POST'])
-def read_recommend_stock_odds():
+@app.route('/read_recommended_stock', methods=['GET', 'POST'])
+def read_recommended_stock():
     days = request.form.get('days_dropdown')
     odds = request.form.get('odds_dropdown')
     odds = float(odds) / 10
     recommended_stock_list = premium_member.read_recommended_stock(days, odds)
     return render_template('query_recommended_stock_page.html', recommended_stock_list=recommended_stock_list)
+
+
+@app.route('/query_specific_stock_page')
+def query_specific_stock_page():
+    return render_template('query_specific_stock_page.html')
+
+
+@app.route('/read_stock_odds', methods=['GET', 'POST'])
+def read_stock_odds():
+    stock_id = request.form.get('stock_id')
+    stock_id = int(stock_id)
+    stock_odds_list = premium_member.read_stock_odds(stock_id)
+    return render_template('query_specific_stock_page.html', stock_odds_list=stock_odds_list)
 
 
 if __name__ == '__main__':
