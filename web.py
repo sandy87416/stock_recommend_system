5323ad6cca5bf450b25ae33dfc9a7cf087fb116f
109598053 <t109598053@ntut.org.tw>
2022-05-04 02:58:32 +0800

    revised query_recommended_stock_page
    revised query_recommended_stock_page with bootstrap-table

--- web.py
+++ web.py
@@ -1,6 +1,4 @@
-from json import dump
-
-from flask import Flask, render_template, request, json, jsonify
+from flask import Flask, render_template, request, jsonify
 from flask_bootstrap import Bootstrap
 
 from model.member.premium_member import PremiumMember
@@ -22,23 +20,25 @@ def stock_odds_menu():
 
 @app.route('/query_recommended_stock_page')
 def query_recommended_stock_page():
-    return render_template('query_recommended_stock_page.html', recommended_stock_list='')
+    return render_template('query_recommended_stock_page.html')
 
 
-@app.route('/read_recommended_stock', methods=['POST'])
+@app.route('/read_recommended_stock', methods=['GET', 'POST'])
 def read_recommended_stock():
-    days = request.form.get('days_dropdown')
-
-    odds = request.form.get('odds_dropdown')
-    odds = float(odds) / 10
-    recommended_stock_list = premium_member.read_recommended_stock(days, odds)
-    data = list()
-    for i in range(len(recommended_stock_list)):
-        d = dict()
-        d['id'] = i+1
-        d['content'] = recommended_stock_list[i]
-        data.append(d)
-    return render_template('query_recommended_stock_page.html', recommended_stock_list=data)
+    days = int(request.values.get('days_dropdown'))
+    odds = int(request.values.get('odds_dropdown'))
+    limit = request.values.get('limit', 10)
+    offset = request.values.get('offset', 1)
+    odds = int(odds) / 10
+    # recommended_stock_list = premium_member.read_recommended_stock(days, odds)
+    recommended_stock_list = [{'stock_id': 8080, 'stock_name': '永利聯合', 'rsi_6': 100.0, 'odds': 0.93},
+                              {'stock_id': 8477, 'stock_name': '創業家', 'rsi_6': 75.61, 'odds': 0.89},
+                              {'stock_id': 4712, 'stock_name': '南璋', 'rsi_6': 60.71, 'odds': 0.93},
+                              {'stock_id': 9802, 'stock_name': '鈺齊-KY', 'rsi_6': 92.0, 'odds': 0.93}]
+    for i in range(20):
+        recommended_stock_list.append(recommended_stock_list[i % 4])
+    json_data = jsonify({'total': len(recommended_stock_list), 'rows': recommended_stock_list[int(offset):(int(offset) + int(limit))]})
+    return json_data
 
 
 @app.route('/query_specific_stock_page')
