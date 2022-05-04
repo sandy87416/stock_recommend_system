6e5e7650d065773d1151dde3ad31c8155862c7e3
109598053 <t109598053@ntut.org.tw>
2022-05-04 22:18:37 +0800

    revised query_specific_stock_page
    revised query_specific_stock_page.html and web.py

--- web.py
+++ web.py
@@ -29,8 +29,14 @@ def read_recommended_stock():
     odds = int(request.values.get('odds'))
     limit = request.values.get('limit', 10)
     offset = request.values.get('offset', 1)
+    recommended_stock_list = [{'stock_id': 8080, 'stock_name': '永利聯合', 'rsi_6': 100.0, 'odds': 0.93},
+                              {'stock_id': 8477, 'stock_name': '創業家', 'rsi_6': 75.61, 'odds': 0.89},
+                              {'stock_id': 4712, 'stock_name': '南璋', 'rsi_6': 60.71, 'odds': 0.93},
+                              {'stock_id': 9802, 'stock_name': '鈺齊-KY', 'rsi_6': 92.0, 'odds': 0.93}]
+    for i in range(days * odds):
+        recommended_stock_list.append(recommended_stock_list[i % 4])
     odds = int(odds) / 10
-    recommended_stock_list = premium_member.read_recommended_stock(days, odds)
+    # recommended_stock_list = premium_member.read_recommended_stock(days, odds)
     json_data = jsonify({'total': len(recommended_stock_list), 'rows': recommended_stock_list[int(offset):(int(offset) + int(limit))]})
     return json_data
 
@@ -42,10 +48,13 @@ def query_specific_stock_page():
 
 @app.route('/read_stock_odds', methods=['GET', 'POST'])
 def read_stock_odds():
-    stock_id = request.form.get('stock_id')
+    stock_id = request.values.get('stock_id')
+    limit = request.values.get('limit', 10)
+    offset = request.values.get('offset', 1)
     stock_id = int(stock_id)
     stock_odds_list = premium_member.read_stock_odds(stock_id)
-    return render_template('query_specific_stock_page.html', stock_odds_list=stock_odds_list)
+    json_data = jsonify({'total': len(stock_odds_list), 'rows': stock_odds_list[int(offset):(int(offset) + int(limit))]})
+    return json_data
 
 
 if __name__ == '__main__':
