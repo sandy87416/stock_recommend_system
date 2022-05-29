c6461a56b38395a5e83cc3bd25b4879183d045c1
109598087 <69856059+109598087@users.noreply.github.com>
2022-05-30 02:19:34 +0800

    added test TestPremiumMember TestOrdinaryMember

--- test/test_model/test_member/test_ordinary_member.py
+++ test/test_model/test_member/test_ordinary_member.py
@@ -75,3 +75,47 @@ class TestOrdinaryMember(TestCase):
         self.assertEqual(stock_intraday_information.get_high_price(), 47.3)
         self.assertEqual(stock_intraday_information.get_low_price(), 46.5)
         self.assertEqual(stock_intraday_information.get_close_price(), 46.5)
+
+    def test_add_selected_stock(self):
+        selected_stock_list = self.ordinary_member.add_selected_stock(2330)
+        self.assertEqual(selected_stock_list[0].get_account(), "t109598087@ntut.org.tw")
+        self.assertEqual(selected_stock_list[0].get_stock_id(), 2330)
+
+        # teardown
+        selected_stock_df = pd.read_csv(database_path + 'selected_stock.csv')
+        selected_stock_df = selected_stock_df.drop(selected_stock_df[
+                                                       (selected_stock_df['account'] == 't109598087@ntut.org.tw') & (
+                                                               selected_stock_df['stock_id'] == 2330)].index)
+        selected_stock_df.to_csv(database_path + 'selected_stock.csv', index=False)
+
+    def test_read_selected_stock(self):
+        self.ordinary_member.add_selected_stock(2330)
+        selected_stock_list = self.ordinary_member.read_selected_stock()
+        self.assertEqual(selected_stock_list[0].get_account(), "t109598087@ntut.org.tw")
+        self.assertEqual(selected_stock_list[0].get_stock_id(), 2330)
+        # teardown
+        selected_stock_df = pd.read_csv(database_path + 'selected_stock.csv')
+        selected_stock_df = selected_stock_df.drop(selected_stock_df[
+                                                       (selected_stock_df['account'] == 't109598087@ntut.org.tw') & (
+                                                               selected_stock_df['stock_id'] == 2330)].index)
+        selected_stock_df.to_csv(database_path + 'selected_stock.csv', index=False)
+
+    def test_delete_selected_stock(self):
+        self.ordinary_member.add_selected_stock(2330)
+        selected_stock_list = self.ordinary_member.read_selected_stock()
+        self.assertEqual(selected_stock_list[0].get_account(), "t109598087@ntut.org.tw")
+        self.assertEqual(selected_stock_list[0].get_stock_id(), 2330)
+
+        self.ordinary_member.delete_selected_stock(2330)
+        selected_stock_list = self.ordinary_member.read_selected_stock()
+        account_list = [selected_stock.get_account() for selected_stock in selected_stock_list]
+        stock_id_list = [selected_stock.get_stock_id() for selected_stock in selected_stock_list]
+        self.assertFalse("t109598087@ntut.org.tw" in account_list)
+        self.assertFalse(2330 in stock_id_list)
+
+    def test_calculate_current_profit_and_loss(self):
+        self.assertEqual(self.ordinary_member.calculate_current_profit_and_loss(2330, 500, 10, 0.6), 584)
+
+    def test_read_stock_classification(self):
+        stock_class_dict = self.ordinary_member.read_stock_classification()
+        self.assertEqual(stock_class_dict['油電燃氣業'][0], '山隆 2616')
