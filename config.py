4be25ae33ced799410fca333ccf1c7d159868995
109598087 <69856059+109598087@users.noreply.github.com>
2022-06-16 00:13:08 +0800

    改用最新資料

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-root_path = 'F:/Courses/110-2/OOAD/'
-database_path = root_path + 'stock_recommend_system/database/'
-# database_path = root_path + 'stock/data/'
+root_path = 'D:/'
+# database_path = root_path + 'stock_recommend_system/database/'
+database_path = root_path + 'stock/data/'
